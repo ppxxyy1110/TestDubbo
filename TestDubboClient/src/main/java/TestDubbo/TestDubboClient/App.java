75be05eb8b00d7e5caa6ad766adce72b9a1e53af
Bill Pan <334067072@qq.com>
2018-04-24 00:28:40 +0800

    code initial

---
+++ TestDubboClient/src/main/java/TestDubbo/TestDubboClient/App.java
@@ -0,0 +1,23 @@
+package TestDubbo.TestDubboClient;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.alibaba.dubbo.demo.DemoService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{"dubbo-demo-consumer.xml"});
+        context.start();
+        // obtain proxy object for remote invocation
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        // execute remote invocation
+        String hello = demoService.sayHello("world");
+        // show the result
+        System.out.println(hello);
+    }
+}
---
+++ TestDubboServer/src/main/java/com/alibaba/dubbo/demo/provider/DemoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.alibaba.dubbo.demo.provider;
+
+import com.alibaba.dubbo.demo.DemoService;
+
+public class DemoServiceImpl implements DemoService{
+
+	public String sayHello(String name) {
+		return "hi i am here " + name;
+	}
+
+}
